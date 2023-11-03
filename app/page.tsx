47ddba303c5488b3c79022c61cc1d99db017b77f
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-11-03 13:08:03 +0200

    add seconds circle

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,50 @@
 import classNames from "classnames";
-import { transform } from "next/dist/build/swc";
 
 export default function Home() {
   const diallines = [];
-
-  // for (let index = 0; index < 60; index += 1) {
-  //   // const diallinesStyle = classNames("diallines", {
-  //   //   "rotate-[]": index,
-  //   // });
-  //   const element = (
-  //     <div className={`diallines rotate-[${6 * index}deg]`}></div>
-  //   );
-  //   diallines.push(element);
-  // }
-
   for (let i = 0; i < 60; i += 1) {
     diallines.push(i);
   }
-  return (
-    <div className=" min-h-screen flex items-center justify-center">
-      <div className=" rounded-[50%] h-[300px] w-[300px] bg-red-400 border-8 border-red-700 relative">
-        {diallines.map((i, idx) => {
-          const rotation = `rotate-[${6 * idx}deg]`;
-          // const rotation = `rotate-[10deg]`;
 
+  return (
+    <div className=" min-h-screen flex items-start justify-end px-5 py-5">
+      <div
+        className={`rounded-[50%_25%] h-[140px] w-[140px] bg-slate-300 relative`}
+      >
+        {diallines.map((i, idx) => {
           const diallineStyle = classNames(
-            `${rotation} origin-[50%_150px] mt-[-8px] left-1/2 w-[2px] h-[15px] z-[2] absolute bg-slate-800 `
+            "origin-[50%_61px] mt-[9px] ml-[-0.5px] left-1/2 w-[1px] h-[6px] z-[2] absolute bg-blue-900",
+            "after:content-[''] after:absolute after:top-0 after:right[-6px] after:rotate-[3deg] after:block after:z-[-1]",
+            "after:h-[4.5px] after:w-[8px] after:bg-black after:rounded-[0_25%_25%_0] after:-mt-[7px] after:bg-blue-900",
+            {
+              "bg-transparent": i === 0 || i % 5 === 0,
+            }
           );
+
+          const numbersStyle = classNames("numbers-hide text-[5px]", {
+            "numbers-show text-center -ml-[2.5px]": i === 0 || i % 5 === 0,
+          });
           return (
             <div
               key={idx}
               style={{ transform: `rotate(${i * 6}deg)` }}
               className={diallineStyle}
-            ></div>
+            >
+              <div className={numbersStyle}>
+                {i.toString().padStart(2, "0")}
+              </div>
+            </div>
           );
         })}
-
-        {/* <div className="triangle-border  ">
-          <div className=" bg-black"></div>
-        </div> */}
+        <div
+          className=" relative w-full h-full rounded-[50%] border-[3px] border-amber-950 z-10
+        before:absolute before:content-[''] before:h-[128px] before:w-[128px] before:rounded-[50%] before:border-2 before:border-slate-300
+        before:top-[3px] before:left-[3px] before:z-10 before:block
+        "
+        ></div>
       </div>
     </div>
   );
 }
+
+// before:border-[2px] before:border-amber-950 before:rounded-[50%] before:block before:z-20
